3a62233deaf3c4332a5998d6e35c0321e647d3a5
Marat Abdurakhmanov <abdurakhmanov.marat1997@gmail.com>
2024-03-02 20:30:37 +0300

    first commit

---
+++ 283.96f3f144d73ace218141.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[283],{8689:(e,n,t)=>{t.r(n),t.d(n,{default:()=>u});var l=t(8799),a=t(7559),r=t(6540);const u=function(){return r.createElement(l.$,{header:r.createElement(a.Fp,null),main:r.createElement(a.RR,null)})}}}]);
